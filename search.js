4c96107c40f416f689d76bfdecf0781928483270
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-01-23 23:10:31 -0500

    feat: Se implemento la conexión con la API artists y la funcionalidad del Search

--- search.js
+++ search.js
@@ -1,23 +1,56 @@
 const resultArtist = document.getElementById("result-artist");
 const playlistContainer = document.getElementById("result-playlists");
+const errorMessageContainer = document.getElementById(
+	"error-message-container"
+);
+const errorMessageTitle = document.getElementById("error-search-title");
 const searchInput = document.getElementById("search-input");
 
 function requestApi(searchTerm) {
-	fetch(`http://localhost:5500/artists?name_like=${searchTerm}`)
-		.then((response) => response.json())
-		.then((results) => displayResults(results));
+	fetch("https://api.jsonbin.io/v3/b/65b04c26266cfc3fde7f0469", {
+		method: "GET",
+		headers: {
+			"X-Access-Key":
+				"$2a$10$.taFhNIZLe0Fme24ECrtc.VXY7bZFpEfa0tf7zvqpUZH/7i4UQm3u",
+		},
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		})
+		.then((data) => {
+			const filteredArtists = data.record.artists.filter(
+				(artist) =>
+					artist.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+					artist.genre.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+			if (filteredArtists.length === 0) {
+				resultArtist.classList.add("hidden");
+				errorMessageContainer.classList.remove("hidden");
+				errorMessageTitle.innerText = `No se ha encontrado ningún resultado para "${searchTerm}"`;
+			} else {
+				displayResults(filteredArtists);
+			}
+		})
+		.catch((error) => {
+			console.error("Error fetching data:", error);
+		});
 }
 
 function displayResults(results) {
-	hidePlaylists();
-	const artistImage = document.getElementById("artist-img");
-	const artistName = document.getElementById("artist-name");
+	if (results.length > 0) {
+		const artistImage = document.getElementById("artist-img");
+		const artistName = document.getElementById("artist-name");
+		const artistGenre = document.getElementById("artist-genre");
 
-	results.forEach((element) => {
-		artistImage.src = element.urlImg;
-		artistName.innerText = element.name;
-	});
-	resultArtist.classList.remove("hidden");
+		results.forEach((element) => {
+			artistImage.src = element.urlImg;
+			artistName.innerText = element.name;
+			artistGenre.innerText = element.genre;
+		});
+	}
 }
 
 function hidePlaylists() {
@@ -25,11 +58,15 @@ function hidePlaylists() {
 }
 
 searchInput.addEventListener("input", function () {
-	const searchTerm = searchInput.value.toLowerCase();
+	const searchTerm = searchInput.value.toLowerCase().trim();
+
 	if (searchTerm === "") {
 		resultArtist.classList.add("hidden");
 		playlistContainer.classList.remove("hidden");
 		return;
 	}
+	playlistContainer.classList.add("hidden");
+	errorMessageContainer.classList.add("hidden");
+	resultArtist.classList.remove("hidden");
 	requestApi(searchTerm);
 });
